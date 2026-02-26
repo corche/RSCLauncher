9565f267e23ff38b12f4779253eb96656f6ee1f4
corche <corche00@gmail.com>
2026-02-26 15:29:56 +0900

    일단 설치&접속은 됨ㅇㅇ

--- app/assets/js/distromanager.js
+++ app/assets/js/distromanager.js
@@ -1,17 +1,19 @@
-const { DistributionAPI } = require('helios-core/common')
+const { DistributionAPI } = require("helios-core/common");
 
-const ConfigManager = require('./configmanager')
+const ConfigManager = require("./configmanager");
 
 // Old WesterosCraft url.
 // exports.REMOTE_DISTRO_URL = 'http://mc.westeroscraft.com/WesterosCraftLauncher/distribution.json'
-exports.REMOTE_DISTRO_URL = 'https://cdn.mysticred.space/launcher/distribution.json'
+exports.REMOTE_DISTRO_URL =
+	"https://raw.githubusercontent.com/corche/mods/refs/heads/main/distribution.json";
+// "https://cdn.mysticred.space/launcher/distribution.json";
 
 const api = new DistributionAPI(
-    ConfigManager.getLauncherDirectory(),
-    null, // Injected forcefully by the preloader.
-    null, // Injected forcefully by the preloader.
-    exports.REMOTE_DISTRO_URL,
-    false
-)
+	ConfigManager.getLauncherDirectory(),
+	null, // Injected forcefully by the preloader.
+	null, // Injected forcefully by the preloader.
+	exports.REMOTE_DISTRO_URL,
+	false
+);
 
-exports.DistroAPI = api
+exports.DistroAPI = api;
